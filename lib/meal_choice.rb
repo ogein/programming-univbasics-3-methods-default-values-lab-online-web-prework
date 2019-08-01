b9105f8207a32ce941543e5b44f02943d91a28be
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-01 23:54:58 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 # Your code here
 def meal_choice(protein = "meat", veg1, veg2)
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
